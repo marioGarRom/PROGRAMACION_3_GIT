54c0aee8b005418dbfb66907630e1f3510f9c7f6
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-09-07 20:55:03 -0500

    fecha

--- src/HOLA.java
+++ src/HOLA.java
@@ -1,5 +1,6 @@
 public class HOLA {
     public static void main(String[] args) {
         System.out.print("HOLA MUNDO");
+        System.out.println("00/22");
     }
 }
---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        Persona p1=new Persona("MARIO",19,"M","ATLACOMULCO");
+        Estudiante e1=new Estudiante("YESSI",19,"F","ATLACOMULCO","839947","Paradigmas","A");
+        e1.mostrarInfo();
+        p1.mostrarInfo();
+    }
+}
