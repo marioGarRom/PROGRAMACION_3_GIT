fc567cf1f0e1b549e36e93db1a040f58f97929f9
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-10-06 08:11:33 -0500

    It is the sum of two numbers with a different input and output interface

--- src/SegPar/Suma.java
+++ src/SegPar/Suma.java
@@ -10,7 +10,6 @@ public class Suma {
         // convierte las entradas String en valores int para usarlos en un cálculo
         int numero1 = Integer.parseInt(primerNumero);
         int numero2 = Integer.parseInt(segundoNumero);
-
         int suma = numero1 + numero2;
         // muestra los resultados en un diálogo de mensajes de JOptionPane
         JOptionPane.showMessageDialog(null, "La sumaes" + suma, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);
