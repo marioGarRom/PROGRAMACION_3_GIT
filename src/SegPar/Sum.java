83a43d4d2d64d5f44165bc115a05a545f005e692
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-10-12 16:03:28 -0500

    It is the sum of two numbers with a different input and output interface

---
+++ src/SegPar/Sum.java
@@ -0,0 +1,17 @@
+package SegPar;
+
+import javax.swing.JOptionPane;
+
+public class Sum {
+    public static void main(String[] args) {
+
+        String firstNumber = JOptionPane.showInputDialog("Enter the first integer");
+        String secondNumber = JOptionPane.showInputDialog("Enter the second integer");
+        // convert String inputs to int values to use in a calculation
+        int number1 = Integer.parseInt(firstNumber);
+        int number2 = Integer.parseInt(secondNumber);
+        int sum = number1 + number2;
+        // display the results in a JOptionPane message dialog
+        JOptionPane.showMessageDialog(null, "The sum is" + sum, "Sum of two integers", JOptionPane.PLAIN_MESSAGE);
+    }
+}// end class Sum
