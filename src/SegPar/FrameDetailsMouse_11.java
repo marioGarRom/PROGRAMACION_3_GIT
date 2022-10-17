67caa9d05a8fa35e7750fe6bde694b65dca12755
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-10-17 17:24:37 -0500

    is a class dealing with an interface

---
+++ src/SegPar/FrameDetailsMouse_11.java
@@ -0,0 +1,37 @@
+package SegPar;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FrameDetailsMouse_11 extends JFrame {
+    private String details; // String displayed in StatusBar
+    private final JLabel statusBar; // JLabel that appears at the bottom of the window
+
+    // constructor sets String of title bar and registers mouse listener component
+    public FrameDetailsMouse_11() {
+        super("Clicks and mouse buttons");
+        statusBar = new JLabel("Click the mouse");
+        add(statusBar, BorderLayout.SOUTH);
+        addMouseListener(new MouseClickHandler()); // add handler
+    }
+
+    // inner class to handle mouse events
+    private class MouseClickHandler extends MouseAdapter {
+        // handle mouse click event and determine which button was pressed
+        @Override
+        public void mouseClicked(MouseEvent event) {
+            int xPos = event.getX(); // get mouse position x
+            int yPos = event.getY(); // get mouse y position
+
+            details = String.format("Clicked %d time(s)", event.getClickCount());
+            if (event.isMetaDown()) // right mouse button
+                details +="with the right mouse button" ;
+            else if (event.isAltDown()) // middle mouse button
+                details += "with the middle mouse button";
+            else // left mouse button
+                details += "with the left mouse button";
+            statusBar.setText(details); // display message in statusbar
+        }
+    }
+} // end class MouseDetailFrame
---
+++ src/SegPar/TestDetailsMouse_11.java
@@ -0,0 +1,12 @@
+package SegPar;
+
+import javax.swing.*;
+
+public class TestDetailsMouse_11 {
+    public static void main(String[] args) {
+        FrameDetailsMouse_11 frameDetailsMouse = new FrameDetailsMouse_11();
+        frameDetailsMouse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameDetailsMouse.setVisible(true);
+        frameDetailsMouse.setSize(400, 150);
+    }
+} // end class MouseDetails
