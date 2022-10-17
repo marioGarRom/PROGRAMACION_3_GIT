13fa50db08ea97aed0339f208aee4cbcc6c94100
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-10-17 18:23:54 -0500

    is a class dealing with an interface

---
+++ src/SegPar/FramePanel_17.java
@@ -0,0 +1,28 @@
+package SegPar;
+// Uso de un objeto JPanel para ayudar a distribuir los componentes.
+
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class FramePanel_17 extends JFrame {
+    private final JPanel buttonPanel; // panel containing the buttons
+    private final JButton[] buttons;
+
+    // constructor with no arguments
+    public FramePanel_17() {
+        super("Panel Demo");
+        buttons = new JButton[5];
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1, buttons.length));
+
+        // create and add the buttons
+        for (int count = 0; count < buttons.length; count++) {
+            buttons[count] = new JButton("Button " + (count + 1));
+            buttonPanel.add(buttons[count]); // add the button to the panel
+        }
+        add(buttonPanel, BorderLayout.SOUTH); // add panel to JFrame
+    }
+} // end FramePanel class
---
+++ src/SegPar/TestDemoPanel_17.java
@@ -0,0 +1,14 @@
+package SegPar;
+// Prueba de MarcoPanel.
+
+import javax.swing.*;
+
+public class TestDemoPanel_17 extends JFrame {
+    public static void main(String[] args) {
+        FramePanel_17 framePanel = new FramePanel_17();
+        framePanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        framePanel.setSize(450, 200);
+        framePanel.setVisible(true);
+    }
+} // end DemoPanel class
+
