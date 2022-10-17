099e30ddc69b057eae819b6c065de6d3c0e4c206
MARIO GARDUÑO ROMERO <mariogarduno523@gmail.com>
2022-10-17 17:45:11 -0500

    is a class dealing with an interface

---
+++ src/SegPar/TestDemoKeys_13.java
@@ -0,0 +1,13 @@
+package SegPar;
+
+// Prueba de MarcoDemoTeclas.
+import javax.swing.JFrame;
+
+public class TestDemoKeys_13 {
+    public static void main(String[] args) {
+        FrameDemoKeys_13 frameDemoKeys = new FrameDemoKeys_13();
+        frameDemoKeys.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameDemoKeys.setSize(350, 100);
+        frameDemoKeys.setVisible(true);
+    }
+} // end KeyDemo class
